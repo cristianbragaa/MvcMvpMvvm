e4b8069233b45a2317049705c50dfd62e8b7b587
Cristian Braga <cristianbraga29@gmail.com>
2024-10-23 13:10:54 -0300

    correção do bug de exibição de usuarios

--- app/src/main/java/com/cristian/mvcmpvmvvm/model/api/UsuarioApiImpl.kt
+++ app/src/main/java/com/cristian/mvcmpvmvvm/model/api/UsuarioApiImpl.kt
@@ -1,15 +1,17 @@
 package com.cristian.mvcmpvmvvm.model.api
 
 import com.cristian.mvcmpvmvvm.model.Usuario
-import com.cristian.mvcmpvmvvm.presenter.UsuarioApi
 
 class UsuarioApiImpl {
     fun exibirUsuarios(): List<Usuario> {
+
         val listaUsuarios = listOf(
             Usuario("Jamilton", 25),
             Usuario("Pedro", 35),
             Usuario("Maria", 55),
             Usuario("Ana", 18),
+            Usuario("Joao", 28),
+            Usuario("Jeane", 55),
         )
         return listaUsuarios
     }
